dcfdf614833e5e265fca0bdb90935c93dbd3b9e9
damatiel <angel_mej05@hotmail.com>
2020-05-08 20:01:46 -0500

    Update ges_punto.php

--- php/ges_punto.php
+++ php/ges_punto.php
@@ -14,9 +14,18 @@ if (isset($_POST["crearpunto"])) {
 	$n1 = $_POST['numero_direc'];
 	$n2 = $_POST['numero2_direc'];
 	$n3 = $_POST['numero3_direc'];
-	$dire = '<p>'.$td.$n1.'#'.$n2.'-'.$n3.'</p>';
-	echo $dire;
+	$dire = $td.$n1.'#'.$n2.'-'.$n3;
+	$query ="INSERT INTO puntos (dir,estado,doc_suscriptor,fecha_act) VALUES ('$dire',1,'$doc',NOW())";
+	$query_exec = mysqli_query($db->conectar(),$query)or die("no se puede realizar la consulta");
+
+	echo "
+				<script>
+				alert('Se añadio el punto de forma correcta');
+				redir('puntos.php');
+				</script>
+				";
 }
+
 if (isset($_POST['buscarpunto'])) {
 	echo "buscarpunto";
 }
