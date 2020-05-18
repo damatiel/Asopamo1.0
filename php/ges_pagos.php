b0124a7294472c69b5a93f039dade5ab50880c74
Jorica <53978220+Jorica@users.noreply.github.com>
2020-05-17 22:01:58 -0500

    1

--- php/ges_pagos.php
+++ php/ges_pagos.php
@@ -37,16 +37,17 @@ require_once __DIR__ . '/conectar.php';
          $numFactura = $_POST['txtNumeroFactura'];
          $id_pagos = $_POST['select'];
          
-        $query = "UPDATE facturacion set total_pagar = '0' WHERE numero_fact = $numFactura";
+        $query = "UPDATE facturacion set total_pagar = '0', saldo_ant = '0' WHERE numero_fact = $numFactura";
         $query_exec = mysqli_query($db->conectar(),$query)or die("no se puede realizar la consulta");
         $query = "SELECT id_punto FROM facturacion WHERE numero_fact = $numFactura";
           $query_exec = mysqli_query($db->conectar(),$query)or die("no se puede realizar la consulta");
              if($fila = mysqli_fetch_array($query_exec)){
                $idPunto = $fila['id_punto'];
                $query ="INSERT INTO pagos (num_factura,id_punto,id_entPago,fecha_pago) VALUES ($numFactura,$idPunto,$id_pagos,NOW())";
-               $query_exec = mysqli_query($db->conectar(),$query)or die("no se puede realizar la consulta");
-               
+               $query_exec = mysqli_query($db->conectar(),$query)or die("no se puede realizar la consulta");  
              }
+             $query = "UPDATE puntos set saldo_ant = '0', contador = '0', descuento = '0' WHERE id = $idPunto";
+             $query_exec = mysqli_query($db->conectar(),$query)or die("no se puede realizar la consulta");
        }
       header('pagos.php');
         
