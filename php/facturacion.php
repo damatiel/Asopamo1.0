1ede2691b5ec8df49c74d790395b78aed67e408d
Jorica <53978220+Jorica@users.noreply.github.com>
2020-06-14 22:46:28 -0500

    Frontxd

--- php/facturacion.php
+++ php/facturacion.php
@@ -28,7 +28,6 @@
     
 </head>
 <body>
-  
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="collapse navbar-collapse bg-primary"; id="">
           <ul class="navbar-nav mr-auto">
@@ -84,7 +83,12 @@
             <h2 class="titulo text-center container">Facturacion</h2>
           </div>
           <br><br>
+          <div class="gridFacturacion">
+          <div class="border-right">
           <form method="post" action="../pdf/pruebacode.php" class="formularioFacturacion">
+          <!--Inicio Primero-->  
+         
+          <div class="text-center"><h6>Consultar Factura Individual</h6></div>
             <div class="container">
                 <div>
                   <label>Direccion</label>
@@ -117,12 +121,26 @@
       </div>
         
     </div>
-              
-              <br>
-              <div class="container text-center">
-                  <label>Periodo De Facturacion</label>
-              </div>
-              <div>
+    <div class="container">
+      <label>Consulta Por ID</label>
+    </div>
+    <div class="container">
+    <input type="number" name="IdPunto" class="form-control" placeholder="ID Punto">
+    </div>
+    <br>
+    <div class="text-center">
+    <button type="submit" name="imprimir1" class="btn btn-success">Imprimir</button>
+    </div>
+    </div>   
+    <!--Fin Primero-->   
+    <!--Inicio Segundo-->   
+    <div class="border-right">
+      <div class="text-center"><h6>Generar Facturas</h6></div>
+    <div class="container">
+    <label>Periodo De Facturacion</label>
+    
+    </div>
+    <div class="container">
                 <select name="mes" class="form-control">
                   <option value="1">Enero</option>
                   <option value="2">Febrero</option>
@@ -137,29 +155,37 @@
                   <option value="11">Noviembre</option>
                   <option value="12">Diciembre</option>
                 </select>
-              </div><br>
-                <div class="container form-group">
-                    <label class="container text-center">Fecha fin de mes</label>
+    </div>
+                <div class="container"><label>Fecha fin de mes</label></div>
+                <div class="form-group">
                   <input type="date" class="form-control documentoSuscriptor" name="fmes" >
                 </div>
-              <div>
-                <input type="number" name="num_inicial" class="form-control" maxlength="3" id="txt" style="text-transform:uppercase;" placeholder="">
+                <div class="container"><label>Rango para generar</label></div>
+                <div class="gridRango">
+              <div class="ml-2 mr-1">
+                <input type="number" name="num_inicial" class="form-control" maxlength="3" id="txt" placeholder="Desde">
               </div>
-              <div>
-                <input type="number" name="num_final" class="form-control" maxlength="1" id="txt" style="text-transform:uppercase;" placeholder="">
+              <div class="ml-1 mr-2">
+                <input type="number" name="num_final" class="form-control" maxlength="1" id="txt" placeholder="Hasta">
+              </div>
               </div>
                 <div class="text-center">
                     <br>
-                    <button type="submit" name="imprimir1" class="btn btn-primary">Imprimir</button>
-                    <button type="submit" name="imprimirt" class="btn btn-success">Imprimir Todos</button>
+                    <button type="submit" name="imprimirt" class="btn btn-success">Generar Facturas</button>
                  </div>
+                 
           </form>
+          </div>
+          <!--Fin Segundo-->
+          <!--Inicio Tercero-->
+          <div class="">
           <form method="post" action="../pdf/prueba2.php" class="formularioFacturacion">
-            <br>
-              <div class="container text-center">
+            
+              <div class="text-center"><h6>Impresion</h6></div>
+              <div class="container">
                   <label>Periodo De Facturacion</label>
               </div>
-              <div>
+              <div class="container">
                 <select name="mes" class="form-control">
                   <option value="1">Enero</option>
                   <option value="2">Febrero</option>
@@ -174,15 +200,23 @@
                   <option value="11">Noviembre</option>
                   <option value="12">Diciembre</option>
                 </select>
-              </div><br>
-                <div class="container form-group">
-                    <label class="container text-center">Fecha fin de mes</label>
+              </div>
+              <div class="container">
+              <label class="text-center">Fecha fin de mes</label>
+              </div>
+                <div class="form-group ml-2 mr-2">
                   <input type="date" class="form-control documentoSuscriptor" name="ultimodia" >
                 </div>
+            <div class="text-center">
             <button type="submit" name="fact4" class="btn btn-success">Excel</button>
             <button type="submit" name="fact3" class="btn btn-success">cortes</button>
             <button type="submit" name="fact2" class="btn btn-success">Imprimir</button>
+            </div>
+           
+            
           </form>
-
+          </div> <!--a-->
+          <!-- Fin Tercero-->
+          
         </body>
         </html>
