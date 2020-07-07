0e007553bff93b02bd8e2b556caf66d06dc7963b
damatiel <angel_mej05@hotmail.com>
2020-07-06 23:50:58 -0500

    s

--- php/respaldo.php
+++ php/respaldo.php
@@ -4,10 +4,23 @@
 	<title></title>
 </head>
 <body>
-	<form method="post" action="restore.php">
-		<input type="file" name="sql">
-		<button type="submit" name="backup">Enviar</button>
+	<form method="post" action="backup.php">
+		<label>Crear respaldo de la base de datos</label>
+		<button type="submit" name="backup">Crear</button>
 	</form>
+	<br>
+	<br>
+	<form method="post" action="restore.php">
+		<label>Subir respaldo de la base de datos</label>
+		<input type="file" name="sql">
+		<button type="submit" name="backup">Subir</button>
+	</form>
+	<br>
+	<br>
+	<form method="post" action="pagos.php">
+		<button type="submit" name="backup">Volver</button>
+	</form>
+
 
 </body>
 </html>
