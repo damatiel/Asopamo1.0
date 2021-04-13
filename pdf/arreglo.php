32788676968862dd7841971f57199a481662fa3d
damatiel <angel_mej05@hotmail.com>
2021-04-13 11:15:38 -0500

    miguel

--- pdf/arreglo.php
+++ pdf/arreglo.php
@@ -1,20 +1,30 @@
 <?php 
 require_once __DIR__ . '/../php/conectar.php';
 
-  $db = new DB_CONNECT();
+$db = new DB_CONNECT();
 
-  session_start();
+session_start();
+$query = "SELECT * FROM valores WHERE id = 6";
+$query_exec = mysqli_query($db->conectar(),$query)or die("no se puede realizar la consultamlinea 8");
+if($fila3 = mysqli_fetch_array($query_exec)){
+  $vinternet= $fila3[2];
 
-  $query = "SELECT * FROM facturacion WHERE  total_pagar >= 100000";
-                  $query_exec = mysqli_query($db->conectar(),$query)or die("no se puede realizar la consulta");
-                  while($fila = mysqli_fetch_array($query_exec)){
-                  	$n_fact = $fila[0];
-                  	$id_punto = $fila[1];
-                  	$nuevo = $fila['total_pagar'];
-                  	$total = $nuevo - 100000;
-                  	$query2 = "UPDATE facturacion set total_pagar = '$total' WHERE numero_fact = $n_fact";
-                  	$query_exec2 = mysqli_query($db->conectar(),$query2)or die("No se pudo conectar linea 16");
-                  	$query2 = "UPDATE puntos set saldo_ant = '$total' WHERE id = $id_punto";
-                  	$query_exec2 = mysqli_query($db->conectar(),$query2)or die("No se pudo conectar linea 18");
-                  }
- ?>
+}
+$query = "SELECT * FROM puntos WHERE  internet = 2";
+$query_exec = mysqli_query($db->conectar(),$query)or die("no se puede realizar la consulta linea 14");
+
+while($fila = mysqli_fetch_array($query_exec)){
+ $id = $fila[0];
+ $mes = 'Abril';
+ $query = "SELECT * FROM facturacion WHERE id_punto = '$id' AND periodo_fact = '$mes'";
+ $query_exec = mysqli_query($db->conectar(),$query)or die("no se puede realizar la consulta linea 19");
+ if($fila2 = mysqli_fetch_array($query_exec)){
+  $total = $fila2['total_pagar'] + $vinternet;
+  $fact = $fila2[0];
+}
+$query2 = "UPDATE facturacion set total_pagar = '$total', internet = '$vinternet' WHERE numero_fact = '$fact'";
+$query_exec2 = mysqli_query($db->conectar(),$query2)or die("No se pudo conectar linea 24");
+$query2 = "UPDATE pagos set total = '$total', internet = '$vinternet' WHERE num_factura = '$fact'";
+$query_exec2 = mysqli_query($db->conectar(),$query2)or die("No se pudo conectar linea 26");
+}
+?>
--- pdf/prueba2.php
+++ pdf/prueba2.php
@@ -46,7 +46,7 @@ $query = "SELECT * FROM facturacion WHERE id_mes = '$mes' AND estado ='1' ORDER 
       $internet = $fila2['internet'];
       $admin_mes1 = "Administración del mes";
       if ($internet > 0) {
-        $admin_mes = $admin_mes + $vinternet;
+        $admin_mes2 = $admin_mes + $vinternet;
         $admin_mes1 = "Admin mes e Internet";
       }
       $dir = $fila2[1]." ".$fila2['indicaciones'];
@@ -88,7 +88,7 @@ $query = "SELECT * FROM facturacion WHERE id_mes = '$mes' AND estado ='1' ORDER 
   <p class="gwd-p-2i7j gwd-p-8lhm gwd-p-z68v">$</p>
   <p class="gwd-p-2i7j gwd-p-1u0r gwd-p-1u5p gwd-p-4sl3" id="SER_PARA">'.$admin_mes.'</p>
   <p class="gwd-p-2i7j gwd-p-8lhm gwd-p-1hqq gwd-p-1dvk">$</p>
-  <p class="gwd-p-2i7j gwd-p-1u0r gwd-p-1u5p gwd-p-5hed gwd-p-fp76" id="SER_PARA_1">'.$admin_mes.'</p>
+  <p class="gwd-p-2i7j gwd-p-1u0r gwd-p-1u5p gwd-p-5hed gwd-p-fp76" id="SER_PARA_1">'.$admin_mes2.'</p>
   <p class="gwd-p-2i7j gwd-p-8lhm gwd-p-1hqq gwd-p-ised gwd-p-1yu7">$</p>
   <p class="gwd-p-2i7j gwd-p-1u0r gwd-p-1u5p gwd-p-5hed gwd-p-3htt gwd-p-1quq" id="OTROS2">'.$otros2.'</p>
   <p class="gwd-p-2i7j gwd-p-8lhm gwd-p-1hqq gwd-p-ised gwd-p-a7vw gwd-p-gf4t">$</p>
