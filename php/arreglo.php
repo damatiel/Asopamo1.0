648c0c8e50cf1297c93ecc7fe1d024514345ba38
damatiel <angel_mej05@hotmail.com>
2021-08-09 22:00:13 -0500

    miguel

--- php/arreglo.php
+++ php/arreglo.php
@@ -15,7 +15,10 @@ while ($fila4 = mysqli_fetch_array($query_exec4)) {
 	// $query = "UPDATE puntos set estado = 5, contador = 5 WHERE saldo_ant >= 16000 AND internet <= 0";
 	// $query_exec = mysqli_query($db->conectar(),$query)or die("no se puede realizar la consulta linea 16");
 
-	$query = "UPDATE puntos set contador = 5 WHERE estado >= 5";
+	// $query = "UPDATE puntos set contador = 5 WHERE estado >= 5";
+	// $query_exec = mysqli_query($db->conectar(),$query)or die("no se puede realizar la consulta linea 18");
+
+	$query = "UPDATE puntos set estado = 3 WHERE contador >= 5 AND saldo_ant >= 1";
 	$query_exec = mysqli_query($db->conectar(),$query)or die("no se puede realizar la consulta linea 18");
 }
 
