93568ba589fb1d597b3c0b42318a94a9bffbc0ad
damatiel <angel_mej05@hotmail.com>
2021-12-07 07:57:20 -0500

    ASOPAMO

--- index.php
+++ index.php
@@ -5,15 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inisio Sesion</title>
-    <!--Boostrap-->
-    <!--Boostrap-->
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-     <!--Boostrap JS-->
-     <script src="../js/jquery-3.5.1.min.js"></script>
-     <script src="../js/popper.min.js"></script>
-     <script src="../js/bootstrap.min.js"></script>
-    <!--C-->
-    <link rel="stylesheet" href="../css/main.css">
+   <!--Boostrap-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <!--Iconos-->
     <link rel="stylesheet" href="css/solid.css">
     <script src="js/all.js"></script>
